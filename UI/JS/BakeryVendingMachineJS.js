706fec214320d2d71eb1684e95db935da89cb307
Dino Capek <dino.capek15@gmail.com>
2022-12-09 15:10:30 +0100

    Initial logic behind logging in is created. Also changed the name of website on all pages.

--- UI/JS/BakeryVendingMachineJS.js
+++ UI/JS/BakeryVendingMachineJS.js
@@ -1,16 +1,36 @@
 $(document).ready(function(){
     console.log("JS is connected.");
+    $("#error").hide();
 
     $("#map").ready(function(){
         console.log("Ready");
+    });
+
+    $("#btnSignIn").click(function(e){
+        e.preventDefault();
+        $("#error").hide();
+        $("#error").empty();
+        if(($("#email").val() != "") && ($("#password").val() != "")){
+            var email = $("#email").val();
+            var password = $("#password").val();
+            if(email == "dcapek@foi.hr" && password == "1234"){
+                window.location.replace("./dashboard.html");
+            }else{
+                $("#error").show();
+                $("#error").append("<span>Incorrect email or password!</span>");
+            }
+        }else{
+            $("#error").show();
+            $("#error").append("<span>Incorrect email or password!</span>");
+        }
     });
 });
 
 function initMap() {
     //Get data from API (APIs URL)
     //Fake API for testing purposes
-    //JSON content of the response: {"a":{"id":"TG_001", "lat":46.22306, "lon":16.12}, "b":{"id":"TG_002", "lat":46.30444, "lon":16.33778}, "c":{"id":"TG_003", "lat":46.24306, "lon":16.20}}
-    var URL = "https://mocki.io/v1/015b79a6-dc0a-4e88-8318-5f64682d1ccd";
+    //JSON content of the response: {"a":{"id":"TG_001", "product":"Baguette", "quantity":"10", "lat":46.22306, "lon":16.12, "online":1}, "b":{"id":"TG_002", "product":"Muffin", "quantity":"7", "lat":46.30444, "lon":16.33778, "online":2}, "c":{"id":"TG_003", "product":"Baguette", "quantity":"15", "lat":46.24306, "lon":16.20, "online":1}}
+    var URL = "https://mocki.io/v1/46d798a1-a31f-41ff-b9b4-dcf925ac5abf";
     $.ajax({type: "GET", url: URL, dataType: "json", complete: function(data){
         var gates = $.parseJSON(data.responseText);
         
@@ -23,16 +43,16 @@ function initMap() {
 
         for(g in gates){
             const location = {lat: gates[g].lat, lng: gates[g].lon};
-            setMarker(location, map, gates[g].id);
+            setMarker(location, map, gates[g].id, gates[g].product, gates[g].quantity, gates[g].online);
         }
 
     }});
     
 }
 
-function setMarker(location, map, gateID){
+function setMarker(location, map, gateID, product, quantity, online){
     var infowindow = new google.maps.InfoWindow({
-        content: "<span>"+gateID+"</span>"
+        content: "<h6>"+gateID+"</h6><span>Product: "+product+"<br>Quantity: "+quantity+"<br><br>Last online: "+online+" min ago</span>"
     });
 
     const marker = new google.maps.Marker({
