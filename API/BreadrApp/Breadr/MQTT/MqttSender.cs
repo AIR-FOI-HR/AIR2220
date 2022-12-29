55f35acb56393a67d7ff271f09abb49dd9211ae1
azugaj <antonio.zugaj99@gmail.com>
2022-12-29 11:25:48 +0100

    Created MQTT sender class

--- API/BreadrApp/Breadr/MQTT/MqttSender.cs
+++ API/BreadrApp/Breadr/MQTT/MqttSender.cs
@@ -1,19 +1,39 @@
-﻿namespace Breadr.MQTT
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+namespace Breadr.MQTT
 {
     public static class MqttSender
     {
-        public static void MqttSend()
+        
+        public static async Task MqttSend(int gateId,string operation)
         {
-            Task.Run(() => {
-                /*
-                var mqttFactory = new MqttFactory();
-                var options = new MqttClientOptionBuilder()
-                .WithClientID()
-                .WithTcpServer()
-                .Build();
-                */
-            });
-            
+  
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+            .WithClientId("API-sender")
+            .WithTcpServer("test.mosquitto.org",1883)
+            .WithCleanSession()
+            .Build();
+
+            await client.ConnectAsync(options);
+
+            string payload= "{\"gate\":\"" + gateId.ToString() + "\",\"operation\": \"" + operation + "\"}";
+
+            var message = new MqttApplicationMessageBuilder()
+            .WithTopic("foi/air2220")
+            .WithPayload(payload)
+            .Build();
+
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(message);
+            }
+
+            await client.DisconnectAsync();
+
         }
 
     }
